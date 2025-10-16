6f9612ddb61c1c8a9f0c4bf381c457a8d78489a3
PRIYA9065 <147863167+PRIYA9065@users.noreply.github.com>
2025-10-17 00:13:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 //your JS code here. If required.
+let value1 = Number(prompt("Enter First Input : " ));
+let value2 = Number(prompt("Enter Second Input : "));
+let final = value1 * 2 + value2;
+alert(final);
